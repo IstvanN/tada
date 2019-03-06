23e3777fd5c2441826c1d2b0b403b35db26f18f8
Istvan Nemeth <nemethistvan.ius@gmail.com>
2019-03-06 18:51:13 +0100

    feat(main): invoke flag parsing in main func

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	printHeader()
+	parseFlags()
+	if lFlag {
+		fmt.Println("gonna list all")
+		return
+	}
 }
 
 func printHeader() {
-	fmt.Println(
+	fmt.Print(
 		`
 Tada - a CLI todo application
 =============================
 
 `)
+}
+
+func showHelp() {
+	printHeader()
+	flag.PrintDefaults()
+	os.Exit(0)
 }
