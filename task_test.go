1b82ed380245a3723089a89241d7520ac2b605f2
Istvan Nemeth <nemethistvan.ius@gmail.com>
2019-03-06 22:46:30 +0100

    test(task): add test for convert tasks to csv func

--- task_test.go
+++ task_test.go
@@ -8,12 +8,30 @@ var tt = []task{
 	*newTask("2"),
 }
 
+var descriptions = []string{"test", "1", "2"}
+
 func TestNewTask(t *testing.T) {
-	expected := []string{"test", "1", "2"}
 	for i, tsk := range tt {
-		if tsk.descr != expected[i] && tsk.done != false {
-			t.Errorf("expected descr: %v expected done: %v, actual descr: %v, actual done: %v",
-				expected[i], false, tsk.descr, tsk.done)
+		if tsk.descr != descriptions[i] && tsk.done != false {
+			t.Errorf("expected descr: %v, expected done: %v, actual descr: %v, actual done: %v",
+				descriptions[i], false, tsk.descr, tsk.done)
 		}
+	}
+}
+
+func TestConvertTasksToCSV(t *testing.T) {
+	actual := convertTasksToCSV(tt)
+
+	var expected string
+	for i, d := range descriptions {
+		if i != len(descriptions)-1 {
+			expected += d + ","
+		} else {
+			expected += d
+		}
+	}
+
+	if actual != expected {
+		t.Errorf("expected: %v but actual: %v", expected, actual)
 	}
 }
