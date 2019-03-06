7c65bb542fbb9233657fbe654d025eea74591d93
Istvan Nemeth <nemethistvan.ius@gmail.com>
2019-03-06 22:24:33 +0100

    fix(task_test): fix name field to descr

--- task_test.go
+++ task_test.go
@@ -6,8 +6,8 @@ func TestNewTask(t *testing.T) {
 	expected := "testing"
 	task := newTask(expected)
 
-	if task.name != expected {
-		t.Errorf("expected: %v, actual: %v", expected, task.name)
+	if task.descr != expected {
+		t.Errorf("expected: %v, actual: %v", expected, task.descr)
 	}
 	if task.done {
 		t.Errorf("expected: %v, actual: %v", expected, task.done)
