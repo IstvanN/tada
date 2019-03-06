a7d7111d6f09dd6b19fb9013fdfedd4e90156935
Istvan Nemeth <nemethistvan.ius@gmail.com>
2019-03-06 23:18:41 +0100

    feat(flag): add -a flag

--- flags.go
+++ flags.go
@@ -5,9 +5,11 @@ import (
 )
 
 var lFlag bool
+var aFlag string
 
 func parseFlags() {
 	flag.Usage = showHelp
 	flag.BoolVar(&lFlag, "l", false, "lists all the tasks")
+	flag.StringVar(&aFlag, "a", "", "adds a new task")
 	flag.Parse()
 }
