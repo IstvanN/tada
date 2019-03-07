d689e2cabb547a428e754c3172d8a592c9256951
Istvan Nemeth <nemethistvan.ius@gmail.com>
2019-03-07 16:21:03 +0100

    feat(flag): add -r to flags

--- flags.go
+++ flags.go
@@ -6,10 +6,12 @@ import (
 
 var lFlag bool
 var aFlag string
+var rFlag int
 
 func parseFlags() {
 	flag.Usage = showHelp
 	flag.BoolVar(&lFlag, "l", false, "lists all the tasks")
 	flag.StringVar(&aFlag, "a", "", "adds a new task")
+	flag.IntVar(&rFlag, "r", 0, "removes the task of the number provided")
 	flag.Parse()
 }
